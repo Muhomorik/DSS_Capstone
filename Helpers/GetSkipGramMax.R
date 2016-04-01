176196115f03b6b12689ef92ae9497aefb7a8a1c
Dmitri Peredera <dmitri.peredera@gmail.com>
2016-04-01 20:27:51 +0300

    must have gone with last commit.

---
+++ Helpers/GetSkipGramMax.R
@@ -0,0 +1,10 @@
+GetSkipGramMax <- function(nGrams){
+  # Get max allowed value for skips for current ngrams
+  #
+  # Args:
+  #   nGrams: NGrams .  
+  
+  #      1  2  3  4  5  6
+  v <- c(0, 4, 2, 1, 0, 0) # 1gram, 2 gram, etc
+  v[nGrams]
+}
